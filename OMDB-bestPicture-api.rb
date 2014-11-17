657b7ee44df7a2ee8d514f72326fff2110b269b4
allisonfitzgerald <allisonfitz@gmail.com>
2014-11-16 21:33:39 -0500

    generalize API response for multiple data points

--- OMDB-bestPicture-api.rb
+++ OMDB-bestPicture-api.rb
@@ -6,12 +6,16 @@ require 'uri'
 
 class Movie
     attr_accessor :title
-    attr_accessor :year
+    attr_accessor :filmYear
     attr_accessor :rating
+    attr_accessor :oscarYear
+    attr_accessor :winner
 
-    def initialize(title, year)
+    def initialize(title, filmYear, oscarYear, winner)
         @title = title
-        @year = year
+        @filmYear = filmYear
+        @oscarYear = oscarYear
+        @winner = winner
     end
 end
 
@@ -53,20 +57,26 @@ def logRatingError(movieTitle, movieRating, errorFilePath)
     errorFile.close
 end
 
-def readFromCSV(movies, csvPath)
+def readFromCSV(movies, csvPath, searchYear)
     moviesFromCSV = CSV.foreach(csvPath) do |row|
-        movies << Movie.new(row[0], row[1])
+        if (row[2].to_i == searchYear)
+            movies << Movie.new(row[0], row[1], row[2], row[3] || false)
+        end
+    end
+
+    movies.each do |m|
+        puts m.title + " " + m.filmYear + " " + m.oscarYear + " " + m.winner.to_s
     end
 end
 
 def queryOMDB(movieList, movieRatings, scriptConfig) 
     movieList.each do |m|
-        blankYear = m.year.to_s.empty?
+        blankYear = m.filmYear.to_s.empty?
 
         if (blankYear)
             queryURI = URI.escape(scriptConfig.apiURL + scriptConfig.titleQuery + m.title)
         else
-            queryURI = URI.escape(scriptConfig.apiURL + scriptConfig.titleQuery + m.title + scriptConfig.yearQuery + m.year)
+            queryURI = URI.escape(scriptConfig.apiURL + scriptConfig.titleQuery + m.title + scriptConfig.yearQuery + m.filmYear)
         end
 
         response = RestClient.get queryURI
@@ -78,7 +88,7 @@ def queryOMDB(movieList, movieRatings, scriptConfig)
             m.rating = parsedResponse[scriptConfig.responseTarget] || "NIL";
 
             if (!scriptConfig.invalidResponses.include? m.rating) 
-                movieRatings.store(m.title, m.rating); 
+                movieRatings.store(m, parsedResponse); 
             else
                 logRatingError(m.title, m.rating, scriptConfig.errorPath)
             end
@@ -86,10 +96,21 @@ def queryOMDB(movieList, movieRatings, scriptConfig)
     end
 end
 
-def sortRatings(movieRatings, outputPath)
-    sortedHash = movieRatings.sort_by { |k, v| v }.reverse
-    outputFile = open(outputPath, 'a')
-    sortedHash.each{ |k, v| outputFile << k + " -- " + v  + "\n" }
+def printMovieData(movieData, scriptConfig)
+    outputFile = open(scriptConfig.outputPath, 'a')
+    outputFile << "Nominee data for the "  + movieData.keys[0].oscarYear + " Annual Academy Awards \n\n"
+    movieData.each { |k, v| 
+        outputFile << k.title + "\n"
+        outputFile << "Rating: " + v["Rated"] + "\n"
+        outputFile << "Release Date: " + v["Released"] + "\n"
+        outputFile << "Genre: " + v["Genre"] + "\n"
+        outputFile << "Director: " + v["Director"] + "\n"
+        if (k.winner)
+            outputFile << "Won the Academy Award for Best Picture\n\n"
+        else
+            outputFile << "\n"
+        end
+    }
     outputFile.close
 end
 
@@ -99,13 +120,18 @@ titleQuery = "?t="
 yearQuery = "&y="
 responseTarget = "imdbRating"
 invalidResponses = [ "N/A", "NIL" ]
-outputPath = "ratingsOutput.txt"
+outputPath = "queryOutput.txt"
 errorPath = "errors.txt"
+
+searchYear = ARGV[0].to_i
+if (searchYear < 1 || searchYear > 86 )
+    abort("Argument must be a valid Oscar year (between 1 and 86)")
+end
 
 scriptConfig = ScriptConfig.new(csvPath, apiURL, titleQuery, yearQuery, responseTarget, invalidResponses, outputPath, errorPath)
 movieList = []
-movieRatings = Hash.new
+movieData = Hash.new
 
-readFromCSV(movieList, scriptConfig.csvPath)
-queryOMDB(movieList, movieRatings, scriptConfig)
-sortRatings(movieRatings, scriptConfig.outputPath)
+readFromCSV(movieList, scriptConfig.csvPath, searchYear)
+queryOMDB(movieList, movieData, scriptConfig)
+printMovieData(movieData, scriptConfig)
